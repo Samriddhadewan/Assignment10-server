80c4d1f733d6cb72d574fc408eac453d904e8420
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-03-03 01:06:33 +0600

    campaign detail section added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middlewares
 app.use(cors());
@@ -36,6 +36,12 @@ async function run(){
       app.get("/campaigns", async(req, res)=>{
         const cursor = campaignDB.find();
         const result = await cursor.toArray();
+        res.send(result);
+      })
+      app.get("/campaigns/:id", async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await campaignDB.findOne(query);
         res.send(result);
       })
 
